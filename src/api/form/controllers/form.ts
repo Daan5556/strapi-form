ec4717c6f946474b6d40f7b379f2e7d1839a665b
daan.eggen <daan.eggen@carux.com>
2025-05-21 13:26:58 +0200

    Added endpoint

--- src/api/form/controllers/form.ts
+++ src/api/form/controllers/form.ts
@@ -5,8 +5,7 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController("api::form.form", () => ({
-  async index(ctx, next) {
-    // called by GET /hello
-    ctx.body = "Hello World!"; // we could also send a JSON
+  async find(ctx, next) {
+    ctx.body = "Hello World!";
   },
 }));
